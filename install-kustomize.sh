504b81442d2c05e28b304983d8f392ad18e0dddb
Peter Farr <Peter@PrismaPhonic.com>
2019-11-07 22:01:42 -0800

    Added a script for installing kustomize

---
+++ install-kustomize.sh
@@ -0,0 +1,12 @@
+opsys=linux  
+curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases |\
+  grep browser_download |\
+  grep $opsys |\
+  cut -d '"' -f 4 |\
+  grep /kustomize/v |\
+  sort | tail -n 1 |\
+  xargs curl -O -L
+tar xzf ./kustomize_v*_${opsys}_amd64.tar.gz
+chmod u+x kustomize
+sudo mv kustomize $GOPATH/bin/kustomize
+rm ./kustomize_v*_${opsys}_amd64.tar.gz
