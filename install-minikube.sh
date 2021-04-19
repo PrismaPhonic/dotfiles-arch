70ab64ccbd085fbcf0907dc0684c5ec0031743f3
Peter Farr <Peter@PrismaPhonic.com>
2021-04-19 04:04:17 -0700

    Switch to paru and no need to update on each install.
    Signed-off-by: Peter Farr <Peter@PrismaPhonic.com>

--- install-minikube.sh
+++ install-minikube.sh
@@ -1,5 +1,5 @@
 # Install packages for KVM
-sudo pacman -Sy qemu qemu-arch-extra libvirt bridge-utils virt-manager ebtables dnsmasq
+sudo pacman -S qemu qemu-arch-extra libvirt bridge-utils virt-manager ebtables dnsmasq
 
 # Add user to necessary groups
 sudo usermod -a -G libvirt $(whoami)
@@ -12,11 +12,11 @@ sudo systemctl start virtlogd.service
 sudo systemctl enable virtlogd.service
 
 # Install docker-machine and kvm2 driver
-sudo pacman -Sy docker-machine
-yay -S docker-machine-driver-kvm2
+sudo pacman -S docker-machine
+paru -S docker-machine-driver-kvm2
 
 # Install minikube
-yay -S minikube-bin
+paru -S minikube-bin
 
 # Restart libvirtd for good measure. Shouldn't have to but seems to help
 sudo systemctl restart libvirtd.service
